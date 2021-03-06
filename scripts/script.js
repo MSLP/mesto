0380c51113adf9d0fbec7caa14f6dd2d9a944f20
Maria Slp <mariiasmagina@gmail.com>
2021-03-06 13:49:39 +0000

    add new photo-element creation

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-//массив данных об изначальном фото-контенте
+//массив данных об изначальном фото-контенте, расположение фото снизу вверх
 const initialElements = [
   {
     name: 'Карачаево-Черкесия',
@@ -26,46 +26,77 @@ const initialElements = [
   }
 ]
 
-//отображение фотографий на странице
-const elements = document.querySelector('.elements');
-const elementTemplate = document.querySelector('#element').content;
-initialElements.forEach(el => {
+// создание html-кода для карточки с фотографией, добавление вперед всех карточек
+function createElement(el) {
   let element = elementTemplate.querySelector('.element').cloneNode(true);
   element.querySelector('.element__img').src = el.link;
   element.querySelector('.element__img').alt = el.name;
   element.querySelector('.element__title').textContent = el.name;
-  elements.append(element);
-});
-
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let popupCloseButton = document.querySelector('.popup__close');
-let inputName = document.querySelector('#input-name');
-let inputDescription = document.querySelector('#input-description');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-let editForm = document.querySelector('.popup__form');
-
-// открытие окна редактирования профиля
-function showPopup() {
-  inputName.value = profileName.textContent;
-  inputDescription.value = profileDescription.textContent;
-  popup.classList.add('popup_opened');
+  elements.prepend(element);
 }
 
-// закрытие окна редактирования профиля
-function closePopup() {
-  popup.classList.remove('popup_opened');
+// отображение начальных карточек с фотографиями на странице
+const elements = document.querySelector('.elements');
+const elementTemplate = document.querySelector('#element').content;
+initialElements.forEach(el => createElement(el));
+
+
+// переменные для работы с окном редактирования профиля
+const editButton = document.querySelector('.profile__edit-button');
+const popupEditProfile = document.querySelector('.popup__edit-profile');
+const inputProfileName = document.querySelector('.popup__input_profile-name');
+const inputDescription = document.querySelector('.popup__input_profile-description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const editCloseButton = document.querySelector('.popup__close_edit-profile');
+const editForm = document.querySelector('.popup__edit-form');
+
+// переменные для работы с окном добавления новой фотографии
+const addButton = document.querySelector('.profile__add-button');
+const popupAddPicture = document.querySelector('.popup__add-picture');
+const inputPhotoName = document.querySelector('.popup__input_photo-name');
+const inputLink = document.querySelector('.popup__input_photo-link');
+const addCloseButton = document.querySelector('.popup__close_add-picture');
+const addForm = document.querySelector('.popup__add-form');
+
+
+// открытие всплывающего окна
+function showPopup(popupName) {
+  //добавление имени и описания в поля, если это окно редактирования профиля
+  if (popupName === popupEditProfile) {
+    inputProfileName.value = profileName.textContent;
+    inputDescription.value = profileDescription.textContent;
+  }
+  popupName.classList.add('popup_opened');
+}
+
+// закрытие всплывающего окна
+function closePopup(popupName) {
+  popupName.classList.remove('popup_opened');
 }
 
 // сохранение формы редактирования профиля
-function savePopup(evt) {
+function saveEditPopup(evt) {
   evt.preventDefault();
-  profileName.textContent = inputName.value;
+  profileName.textContent = inputProfileName.value;
   profileDescription.textContent = inputDescription.value;
-  closePopup();
+  closePopup(popupEditProfile);
 }
 
-editButton.addEventListener('click', showPopup);
-popupCloseButton.addEventListener('click', closePopup);
-editForm.addEventListener('submit', savePopup);
+// сохранение формы добавления новой фотографии
+function saveAddPopup(evt) {
+  evt.preventDefault();
+  let newPhoto = {
+    name: inputPhotoName.value,
+    link: inputLink.value
+  }
+  createElement(newPhoto);
+  closePopup(popupAddPicture);
+}
+
+editButton.addEventListener('click', function() {showPopup(popupEditProfile)});
+addButton.addEventListener('click', function() {showPopup(popupAddPicture)});
+editCloseButton.addEventListener('click', function() {closePopup(popupEditProfile)});
+addCloseButton.addEventListener('click', function() {closePopup(popupAddPicture)});
+editForm.addEventListener('submit', saveEditPopup);
+addForm.addEventListener('submit', saveAddPopup);
